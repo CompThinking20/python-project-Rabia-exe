ccdc1fda752089030f8ad7dc643056595b0e5264
Rabiaexe <rabia.danyaro@rutgers.edu>
2020-12-07 16:38:14 +0000

    Categorized the images and songs so user input can influence what image and song is randomly selected by the program.

--- Main.py
+++ Main.py
@@ -1,14 +1,40 @@
 #This is a List of song names to be selected at random. They are in script form because this is just a placeholder.
-songList = ["Ahabib - Terakraft ", "Awa Adounia - Terakraft ", "Tahalamot - Tinariwen ", "Toumast anlet - Tamikrest "]
+sadSongs = ["Tahalamot - Tinariwen ", "Toumast anlet - Tamikrest ", "Nanuflay - Tinariwen "]
+
+madSongs = [ "Karambani - Terakraft ", "Kamelemba - Oumou Sangare ", "Tahigren - Bombino "]
+
+gladSongs = ["Ahabib - Terakraft ", "Bamako - Songhoy Blues", "Awa Adounia - Terakraft "]
 
 #This is a List of images to be selected at random. It is also just a placeholder.
-imageList = ["img_1", "img_2", "img_3", "img_4"]
+north = ["Nubian architecture in Aswan, Egypt", "Ait Ben Haloud in Morocco", "Berber architecture in Mzab Algeria"]
+west = ["Palace of the Emir of Kano, Nigeria", "Great Mosque of Djenné in Mali ", "Palace of the Emir of Dutse in Kaduna, Nigeria ", "Emir of Zazzau's palace in Kaduna, Nigeria"]
 
+#This asks the user to type either "sad," "mad," or "glad." This influences which song the user might get. 
+print("Feeling sad, mad, or glad?")
+feeling = input()
+#This asks the user which direction they're going which can influence which image they get.
+print("Are you going West or North?")
+direction = input()
+
+#This displays the mood and direction the user has typed in. 
+print("Since you're feeling " + feeling + ", and going " + direction + ",")
 
 import random
-#This makes a random selection from the lists because the final project would have a button to click which would radomly pair an image with a song. 
-songChoice = random.choice(songList)
-imageChoice = random.choice(imageList)
+#The user will recieve a random combination of a song name and image name based on their input.
+if feeling == "sad":
+  songChoice = random.choice(sadSongs)
+
+if feeling == "mad":
+  songChoice = random.choice(madSongs)
+
+if feeling == "glad":
+  songChoice = random.choice(gladSongs)
+
+if direction == "North":
+  imageChoice = random.choice(north)
+
+if direction == "West":
+  imageChoice = random.choice(west)
 
 #This prints the random song and image names to the terminal just to make sure the code is working. 
 print("The song and image pair is: " + songChoice + "and " + imageChoice)
