c6eb472ae4c2bd512ee54549b7ca3f7daf389efa
Rabiaexe <rabia.danyaro@rutgers.edu>
2020-12-16 03:07:48 +0000

    Added more input requests on lines 33 and 45 along with print functions that describe what a scimitar and pirogue are.

--- Main.py
+++ Main.py
@@ -1,40 +1,58 @@
+def main():
 #This is a List of song names to be selected at random. They are in script form because this is just a placeholder.
-sadSongs = ["Tahalamot - Tinariwen ", "Toumast anlet - Tamikrest ", "Nanuflay - Tinariwen "]
+  sadSongs = ["Tahalamot - Tinariwen ", "Toumast anlet - Tamikrest ", "Nanuflay - Tinariwen "]
 
-madSongs = [ "Karambani - Terakraft ", "Kamelemba - Oumou Sangare ", "Tahigren - Bombino "]
+  madSongs = [ "Karambani - Terakraft ", "Kamelemba - Oumou Sangare ", "Tahigren - Bombino "]
 
-gladSongs = ["Ahabib - Terakraft ", "Bamako - Songhoy Blues", "Awa Adounia - Terakraft "]
+  gladSongs = ["Ahabib - Terakraft ", "Bamako - Songhoy Blues", "Awa Adounia - Terakraft "]
 
-#This is a List of images to be selected at random. It is also just a placeholder.
-north = ["Nubian architecture in Aswan, Egypt", "Ait Ben Haloud in Morocco", "Berber architecture in Mzab Algeria"]
-west = ["Palace of the Emir of Kano, Nigeria", "Great Mosque of Djenné in Mali ", "Palace of the Emir of Dutse in Kaduna, Nigeria ", "Emir of Zazzau's palace in Kaduna, Nigeria"]
+  #This is a List of images to be selected at random. It is also just a placeholder.
+  north = ["Nubian architecture in Aswan, Egypt", "Ait Ben Haloud in Morocco", "Berber architecture in Mzab Algeria"]
+  west = ["Palace of the Emir of Kano, Nigeria", "Great Mosque of Djenné in Mali ", "Palace of the Emir of Dutse in Kaduna, Nigeria ", "Emir of Zazzau's palace in Kaduna, Nigeria"]
 
-#This asks the user to type either "sad," "mad," or "glad." This influences which song the user might get. 
-print("Feeling sad, mad, or glad?")
-feeling = input()
-#This asks the user which direction they're going which can influence which image they get.
-print("Are you going West or North?")
-direction = input()
+  #This asks the user to type either "sad," "mad," or "glad." This influences which song the user might get. 
+  feeling = input("Feeling sad, mad, or glad? ")
+  #This asks the user which direction they're going which can influence which image they get.
+  direction = input("Are you going West or North? ")
 
-#This displays the mood and direction the user has typed in. 
-print("Since you're feeling " + feeling + ", and going " + direction + ",")
+  import random
+  #The user will recieve a random combination of a song name and image name based on their input.
+  if feeling == "sad" or feeling == "Sad":
+    songChoice = random.choice(sadSongs)
 
-import random
-#The user will recieve a random combination of a song name and image name based on their input.
-if feeling == "sad":
-  songChoice = random.choice(sadSongs)
+  if feeling == "mad" or feeling == "Mad":
+    songChoice = random.choice(madSongs)
 
-if feeling == "mad":
-  songChoice = random.choice(madSongs)
+  if feeling == "glad" or feeling == "Glad":
+    songChoice = random.choice(gladSongs)
 
-if feeling == "glad":
-  songChoice = random.choice(gladSongs)
+  if direction == "North" or direction == "north":
+    imageChoice = random.choice(north)
+    #If the user answers no to the question, the song and image is given, otherwise, the program ends here.
+    print("Are you prepared for your travel?")
+    prepareNorth = input()
+    if prepareNorth == "no" or prepareNorth == "No":
+      print("Take this scimitar with you!")
+      print("A scimitar is a sword with a curved blade.")
+    else:
+      print("Watch out for the carnivorous camels!")
+      return
 
-if direction == "North":
-  imageChoice = random.choice(north)
+  if direction == "West" or direction == "west":
+    imageChoice = random.choice(west)
+    #If the user answers no to the question, the song and image is given, otherwise, the program ends here.
+    print("Are you prepared for your travel?")
+    prepareWest = input()
+    if prepareWest == "no" or prepareWest == "No":
+      print("Take this pirogue with you!")
+      print("A pirogue is a fishing boat that will help you sail down the river.")
+    else:
+      print("You drowned in the river!")
+      return
 
-if direction == "West":
-  imageChoice = random.choice(west)
+  #This displays the mood and direction the user has typed in. 
+  print("Since you're feeling " + str(feeling) + ", and going " + str(direction) + ",")
+  #This prints the random song and image names to the terminal just to make sure the code is working. 
+  print("The song and location are: " + str(songChoice) + "and " + str(imageChoice))
 
-#This prints the random song and image names to the terminal just to make sure the code is working. 
-print("The song and image pair is: " + songChoice + "and " + imageChoice)
+main()
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 [![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=3684859&assignment_repo_type=AssignmentRepo)
+
+Project Overview
+The big project was going to include a button for the user to click on in a website so they will get a random pairing of a song playing and a displayed image. The little piece I managed to perform was a program that asked the user how they were feeling, which direction they are are going and if they were “prepared for their travel” and based on their answers, they may get a random pairing of a song name and location along with a description of an object that is given for the user for travel. 
+
+Project Narrative
+To reach Milestone 1, I made two lists, one with song names and another with image names. Then used random.choice to have the program print a random combination of a song and image. For Milestone 2, I divided the song names into three lists, “sad”, “glad”, and “mad.” As for the image names, they have been divided into lists named “North” and “South.” The user would be asked about their mood and which direction they’re going, the result in song and image depends on their answers. 
+
+The concepts from the lessons that were useful for this project were how to make lists and if statements. I had to look elsewhere for information on how to use the random.choice method. 
+What surprised me was that I had to type “def main():” in order for the return statements within the if statements to work properly. 
+
+If I were to approach this project again, I would use the if statements and lists to make a story based educational game instead of this song name and location pairing. Perhaps something that would feel like a tour of a place through text. 
+
+Corrections
+Your comment on my first milestone called for more complexity by requesting user input. I added user input by dividing the songs and images into different categories and adding the input function on lines 14 and 16 which ask "Feeling sad, mad, or glad?" and “Are you going West or North?" If the user inputs the string “sad” for example, the program will print a song name from the sad list. If the user inputs “North” to answer the directions question, the program will print an image name from the North list. 
+
+For the second milestone, you mentioned receiving an error after inputting an emotion which states that it is undefined and suggested using the string function for the inputs. I addressed this by adding the string function to lines 54 and 56 to the variables feeling, direction,songChoice, and imageChoice. In lines 20, 23, 26, 29, and 41, the if statements detect if a specific input has been entered. I made two versions of each word, one that is capitalized and the other not capitalized so the program would detect the input as true whether it is capitalized or not. 
+
+You also suggested that I add something to the direction entry so I added another input request under the random.choice functions for the North and West variables which asks if the user is ready for their travel and if they answer "no", the program gives them an item along with a brief description.  
+
